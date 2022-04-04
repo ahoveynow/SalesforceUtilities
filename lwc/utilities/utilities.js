6238d666d4f79eb6bea5c01716544d105d23bd40
Andrew Hovey <andrew@ahovey.com>
2022-04-04 13:47:55 -0400

    Add convertRemToPixels and sleep utilities

---
+++ lwc/utilities/utilities.js
@@ -0,0 +1,25 @@
+/**
+ * Converts a rem value to a pixel value.
+ * @param {Number} rem The rem ("root em") value to be converted.
+ * @returns {Number} The number of pixels after conversion.
+ */
+export const convertRemToPixels = (rem) => {    
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+/**
+ * "Sleeps" for a specified number of milliseconds and resolves.
+ * If you await a call to this method, you can cause a method to pause for various purposes.
+ * Passing in 0 (or allowing default) will simply make the remainder of your method (if you await)
+ * enter the end of the JS event queue, which is helpful for UX and LWC reactive needs.
+ * 
+ * @param {Integer} [msToSleep] The number of milliseconds to sleep
+ * @returns Promise which resolves when msToSleep completes.
+ * 
+ * @example <caption>Wait 1 second before proceeding.</caption>
+ * await sleep(1000);
+ * @example <caption>Shift to the end of the Javascript event queue.</caption>
+ * await sleep();
+ */
+
+export const sleep = (msToSleep = 0) => new Promise((resolve) => setTimeout(resolve, msToSleep));
