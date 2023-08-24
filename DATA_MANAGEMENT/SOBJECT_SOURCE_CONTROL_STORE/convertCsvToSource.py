897eff89a7f166737f540d3b071de1a5c4373e9e
Andrew Hovey <andrew@ahovey.com>
2023-08-24 16:04:13 -0400

    Update convertCsvToSource.py

--- DATA_MANAGEMENT/SOBJECT_SOURCE_CONTROL_STORE/convertCsvToSource.py
+++ DATA_MANAGEMENT/SOBJECT_SOURCE_CONTROL_STORE/convertCsvToSource.py
@@ -4,6 +4,8 @@
 # The above abbreviated copyright notice shall be included in all copies or substantial portions of the Software.
 # -----------------------------------------------------
 
+# https://github.com/ahoveynow/SalesforceUtilities/blob/main/DATA_MANAGEMENT/SOBJECT_SOURCE_CONTROL_STORE
+
 # USAGE:
 # python3 convertCsvToSource.py 
 #   --sourceFile ../Some_Config__c_from_org.csv 
@@ -32,32 +34,32 @@ csvFileRecords = []
 ### PROCESS PARAMS ###
 
 def processParams():
-    global sourceFile, destinationFolder, upsertField, objectName
-    params = util.getArgParams()
+	global sourceFile, destinationFolder, upsertField, objectName
+	params = util.getArgParams()
 
-    # sourceFile
-    sourceFile = ('sourceFile' in params.keys() and params['sourceFile'])
-    if not sourceFile:
-        util.exitWithFailure('You must specify the csv source file with the --sourceFile flag')
-    print(f'sourceFile: {sourceFile}')
+	# sourceFile
+	sourceFile = ('sourceFile' in params.keys() and params['sourceFile'])
+	if not sourceFile:
+		util.exitWithFailure('You must specify the csv source file with the --sourceFile flag')
+	print(f'sourceFile: {sourceFile}')
 
-    # destinationFolder
-    destinationFolder = ('destinationFolder' in params.keys() and params['destinationFolder'])
-    if not destinationFolder:
-        util.exitWithFailure('You must specify the destination folder with the --destinationFolder flag.')
-    print(f'destinationFolder: {destinationFolder}')
+	# destinationFolder
+	destinationFolder = ('destinationFolder' in params.keys() and params['destinationFolder'])
+	if not destinationFolder:
+		util.exitWithFailure('You must specify the destination folder with the --destinationFolder flag.')
+	print(f'destinationFolder: {destinationFolder}')
 
-    # upsertField
-    upsertField = ('upsertField' in params.keys() and params['upsertField'])
-    if not upsertField:
-        util.exitWithFailure('You must specify the Object\'s upsert field with the --upsertField flag')
-    print(f'upsertField: {upsertField}')
+	# upsertField
+	upsertField = ('upsertField' in params.keys() and params['upsertField'])
+	if not upsertField:
+		util.exitWithFailure('You must specify the Object\'s upsert field with the --upsertField flag')
+	print(f'upsertField: {upsertField}')
 
-    # objectName
-    objectName = ('objectName' in params.keys() and params['objectName'])
-    if not objectName:
-        util.exitWithFailure('You must specify the Object\'s API name with the --objectName flag')
-    print(f'objectName: {objectName}')
+	# objectName
+	objectName = ('objectName' in params.keys() and params['objectName'])
+	if not objectName:
+		util.exitWithFailure('You must specify the Object\'s API name with the --objectName flag')
+	print(f'objectName: {objectName}')
 
 
 
@@ -65,20 +67,21 @@ def processParams():
 ### EXTRACT CSV RECORDS ###
 
 def extractCsvRecords():
-    global csvFileRecords
+	global csvFileRecords
 
-    csvFileContent = open(sourceFile, encoding='utf8')
-    csvReader = csv.DictReader(csvFileContent)
-    csvFileRecords = []
-    for csvRow in csvReader:
-        csvRow['__SObjectType'] = objectName
-        csvFileRecords.append(csvRow)
+	csvFileContent = open(sourceFile, encoding='utf8')
+	csvReader = csv.DictReader(csvFileContent)
+	csvFileRecords = []
+	for csvRow in csvReader:
+		csvRow['__SObjectType'] = objectName
+		csvRow['__upsertField'] = upsertField
+		csvFileRecords.append(csvRow)
 
-    # TEMPORARY FIX DUE TO CLI BUG: https://github.com/forcedotcom/cli/issues/1447
-    for record in csvFileRecords:
-        for fieldName in record:
-            if record[fieldName] == 'null':
-                record[fieldName] = ''
+	# TEMPORARY FIX DUE TO CLI BUG: https://github.com/forcedotcom/cli/issues/1447
+	for record in csvFileRecords:
+		for fieldName in record:
+			if record[fieldName] == 'null':
+				record[fieldName] = ''
 
 
 
@@ -86,14 +89,14 @@ def extractCsvRecords():
 ### WRITE RECORDS TO JSON FILES ###
 
 def writeRecordsToJsonFiles():
-    os.makedirs(destinationFolder, exist_ok=True)
+	os.makedirs(destinationFolder, exist_ok=True)
 
-    for record in csvFileRecords:
-        fileName = f'{destinationFolder}/{record[upsertField]}.json'
-        print(f'Writing to {fileName}')
-        fileToWrite = open(fileName, 'w')
-        fileToWrite.write(json.dumps(record, indent = 4, sort_keys = True))
-        fileToWrite.close()
+	for record in csvFileRecords:
+		fileName = f'{destinationFolder}/{record[upsertField]}.json'
+		print(f'Writing to {fileName}')
+		fileToWrite = open(fileName, 'w')
+		fileToWrite.write(json.dumps(record, indent = 4, sort_keys = True))
+		fileToWrite.close()
 
 
 
@@ -101,9 +104,9 @@ def writeRecordsToJsonFiles():
 ### EXECUTE ###
 
 def execute():
-    processParams()
-    extractCsvRecords()
-    writeRecordsToJsonFiles()
+	processParams()
+	extractCsvRecords()
+	writeRecordsToJsonFiles()
 
 
 
